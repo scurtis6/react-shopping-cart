3d29b5a2463370e10459d61496c2ffac740a467b
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-20 13:06:21 -0500

    removed props from function, map and addItem prop

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,14 +4,15 @@ import ProductContext from '../contexts/ProductContext';
 // Components
 import Product from './Product';
 
-const Products = props => {
+const Products = () => {
+	const { products, addItem } = useContext(ProductContext);
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
