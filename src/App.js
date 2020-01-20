89278152f21cc54d60a7681950049b5f459e321f
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-20 12:59:57 -0500

    refactored the routes

--- src/App.js
+++ src/App.js
@@ -28,17 +28,12 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component={Products}
 			/>
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component={ShoppingCart}
 			/>
 			</ProductContext.Provider>
 		</div>
