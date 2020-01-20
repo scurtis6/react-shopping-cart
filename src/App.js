dc930b0e129b257557c34ce7b99a616ad339bd96
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-20 12:50:09 -0500

    create and imported ProductContext

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import ProductContext from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
